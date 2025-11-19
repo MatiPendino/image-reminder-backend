cc586e2d450f78ed52c8e795cf7e880dc8721f6c
Mati <matiaspendino76@gmail.com>
2025-11-19 19:51:15 +0100

    refactor: clean AlarmViewSet

--- ImageReminder/alarm/serializers.py
+++ ImageReminder/alarm/serializers.py
@@ -4,4 +4,5 @@ from .models import Alarm
 class AlarmSerializer(serializers.ModelSerializer):
     class Meta:
         model = Alarm
-        fields = ('id', 'alarm_user',  'title',  'time',  'weekdays',  'image')
+        fields = ['id', 'title', 'time', 'weekdays', 'image']
+        read_only_fields = ['alarm_user']
--- ImageReminder/alarm/views.py
+++ ImageReminder/alarm/views.py
@@ -1,6 +1,5 @@
 from sentry_sdk import capture_message
-from rest_framework import viewsets, status
-from rest_framework.response import Response
+from rest_framework import viewsets
 from .models import Alarm
 from .serializers import AlarmSerializer
 
@@ -11,34 +10,12 @@ class AlarmViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return Alarm.objects.filter(alarm_user=self.request.alarm_user).order_by('time')
 
-    def create(self, request, *args, **kwargs):
-        try:
-            data = request.data
-            data['alarm_user'] = request.alarm_user.id 
+    def perform_create(self, serializer):
+        serializer.save(alarm_user=self.request.alarm_user)
 
-            # Pass the modified data to the serializer
-            serializer = self.get_serializer(data=data)
-            serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
-            headers = self.get_success_headers(serializer.data)
+    def perform_update(self, serializer):
+        serializer.save(alarm_user=self.request.alarm_user)
 
-            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-        except Exception as e:
-            capture_message(str(e), level="error")
-            return Response({'error': 'Error processing the request'}, status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, *args, **kwargs):
-        try:
-            partial = kwargs.pop('partial', False)
-            instance = self.get_object()
-            data = request.data
-            data['alarm_user'] = request.alarm_user.id
-
-            serializer = self.get_serializer(instance, data=data, partial=partial)
-            serializer.is_valid(raise_exception=True)
-            self.perform_update(serializer)
-
-            return Response(serializer.data)
-        except Exception as e:
-            capture_message(str(e), level="error")
-            return Response({'error': 'Error processing the request'}, status=status.HTTP_400_BAD_REQUEST)
+    def handle_exception(self, exc):
+        capture_message(f"AlarmViewSet Exception: {str(exc)}")
+        return super().handle_exception(exc)
